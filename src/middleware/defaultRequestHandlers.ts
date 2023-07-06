6558b89568b2ccd2ae53ce1848550a7fc9537bf7
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-07-06 02:30:13 -0700

    Create reusable defaultRequestHandlers (and use them for users and posts API)

---
+++ src/middleware/defaultRequestHandlers.ts
@@ -0,0 +1,100 @@
+import express, { Request, Response, NextFunction } from "express"
+import mongoose from "mongoose";
+import { AppError } from "./errorHandlers";
+
+export function processRequestGetAll(model: mongoose.Model<any>) {
+    return async function (_req: Request, res: Response, next: NextFunction) {
+        try {
+            const allItems = await model.find()
+            res.type("json").send(JSON.stringify(allItems, null, 2))
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+export function processRequestGetById(model: mongoose.Model<any>) {
+    return async function (req: Request, res: Response, next: NextFunction) {
+        const modelName = model.modelName
+        const id = req.params.id
+    
+        try {
+            const doc = await model.findById(id)
+    
+            if(doc) {
+                res.status(200).type("json").send(JSON.stringify(doc, null, 2))
+            } else {
+                // ToDo [question]: is it OK to create a new Error and use next (expect the error to be logged and response to be sent by one of the following middlewares) instead of sending response res.json?
+                // res.status(404).send(`Cannot find ${modelName} with _ID ${id}`)
+                next(new AppError(404, `Cannot find ${modelName} with _ID ${id}`))
+            }
+        } catch (error: any) {
+            // If error of casting ":id" to MongoDB ObjectId ocurred, then we return 404, because the object with this illegal _ID does not exist
+            if(error.kind === "ObjectId") {
+                // res.status(404).send(`Cannot find ${modelName} with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`)
+                next(new AppError(404, `Cannot find ${modelName} with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`))
+            } else {
+                next(error)
+            }
+        }
+    }
+}
+
+export function processRequestPost(model: mongoose.Model<any>) {
+    return async function (req: Request, res: Response, next: NextFunction) {
+        try {
+            const doc = req.body
+            const createdDoc = await model.create(doc)
+            res.status(201).type("json").send(JSON.stringify(createdDoc, null, 2))
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+export function processRequestPatchById(model: mongoose.Model<any>) {
+    return async function (req: Request, res: Response, next: NextFunction) {
+        const modelName = model.modelName
+        const id = req.params.id
+
+        try {
+            const docUpdates = req.body
+            const docUpdateResponse = await model.findByIdAndUpdate(id, docUpdates, { new: true })
+    
+            if(docUpdateResponse) {
+                res.status(201).type("json").send(JSON.stringify(docUpdateResponse, null, 2))
+            } else {
+                // res.status(404).send(`Cannot find ${modelName} with _ID ${id}`)
+                next(new AppError(404, `Cannot find ${modelName} with _ID ${id}`))
+            }
+        } catch (error: any) {
+            // If error of casting ":id" to MongoDB ObjectId ocurred, then we return 404, because the object with this illegal _ID does not exist
+            if(error.kind === "ObjectId") {
+                // res.status(404).send(`Cannot find ${modelName} with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`)
+                next(new AppError(404, `Cannot find ${modelName} with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`))
+            } else {
+                next(error)
+            }
+        }
+    }
+}
+
+export function processRequestDeleteById(model: mongoose.Model<any>) {
+    return async function (req: Request, res: Response, next: NextFunction) {        
+        try {
+            const id = req.params.id
+            const deletedDoc = await model.findByIdAndDelete(id)
+    
+            const status = deletedDoc ? 200 : 204
+            res.status(status).json("{}")
+        } catch (error: any) {
+            // If error of casting ":id" to MongoDB ObjectId ocurred, then we return 204, because the doc with this ID
+            // cannot exist. Therefore, if the user requests to delete it, we respond that it does not exist (204 No Content)
+            if(error.kind === "ObjectId") {
+                res.status(204).json("{}")
+            } else {
+                next(error)
+            }
+        }
+    }
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,94 +1,15 @@
-import express, { Request, Response, NextFunction } from "express"
+import express from "express"
 import { userModel as User } from "../models/user";
 import { verifyBodyIsNotEmpty } from "../middleware/requestVerifiers";
-import { AppError } from "../middleware/errorHandlers";
+import { processRequestDeleteById, processRequestGetAll, processRequestGetById, processRequestPatchById, processRequestPost } from "../middleware/defaultRequestHandlers";
 
 
 const router = express.Router()
 
-router.get("/", async (_req: Request, res: Response, next: NextFunction) => {
-    try {
-        const allUsers = await User.find()
-        res.type("json").send(JSON.stringify(allUsers, null, 2))
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id
-    
-    try {
-        const user = await User.findById(id)
-
-        if(user) {
-            res.status(200).type("json").send(JSON.stringify(user, null, 2))
-        } else {
-            // ToDo [question]: is it OK to create a new Error and use next (expect the error to be logged and response to be sent by one of the following middlewares) instead of sending response res.json?
-            // res.status(404).send(`Cannot find user with _ID ${id}`)
-            next(new AppError(404, `Cannot find user with _ID ${id}`))
-        }
-    } catch (error: any) {
-        // If error of casting ":id" to MongoDB ObjectId ocurred, then we return 404, because the object with this illegal _ID does not exist
-        if(error.kind === "ObjectId") {
-            // res.status(404).send(`Cannot find user with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`)
-            next(new AppError(404, `Cannot find user with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`))
-        } else {
-            next(error)
-        }
-    }
-})
-
-router.post("/", verifyBodyIsNotEmpty, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = req.body
-        const createdUser = await User.create(user)
-        res.status(201).type("json").send(JSON.stringify(createdUser, null, 2))
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.patch("/:id", verifyBodyIsNotEmpty, async (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id
-    
-    try {
-        const userUpdates = req.body
-        const userUpdateResponse = await User.findByIdAndUpdate(id, userUpdates, { new: true })
-
-        if(userUpdateResponse) {
-            res.status(201).type("json").send(JSON.stringify(userUpdateResponse, null, 2))
-        } else {
-            // res.status(404).send(`Cannot find user with _ID ${id}`)
-            next(new AppError(404, `Cannot find user with _ID ${id}`))
-        }
-    } catch (error: any) {
-        // If error of casting ":id" to MongoDB ObjectId ocurred, then we return 404, because the object with this illegal _ID does not exist
-        if(error.kind === "ObjectId") {
-            // res.status(404).send(`Cannot find user with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`)
-            next(new AppError(404, `Cannot find user with _ID ${id} (illegal ObjectId value, must be 24 character hexadecimal)`))
-        } else {
-            next(error)
-        }
-    }
-})
-
-router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id
-        const deletedUser = await User.findByIdAndDelete(id)
-
-        const status = deletedUser ? 200 : 204
-        res.status(status).json("{}")
-    } catch (error: any) {
-        // If error of casting ":id" to MongoDB ObjectId ocurred, then we return 204, because the object with this ID
-        // cannot exist. Therefore, if the user requests to delete it, we respond that it does not exist (204 No Content)
-        if(error.kind === "ObjectId") {
-            res.status(204).json("{}")
-        } else {
-            next(error)
-        }
-    }
-})
+router.get("/", processRequestGetAll(User))
+router.get("/:id", processRequestGetById(User))
+router.post("/", verifyBodyIsNotEmpty, processRequestPost(User))
+router.patch("/:id", verifyBodyIsNotEmpty, processRequestPatchById(User))
+router.delete("/:id", processRequestDeleteById(User))
 
 export default router
